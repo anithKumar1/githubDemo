55bc6d3280a635f9aabd462a8ca7066045ff6246
anithKumar1 <anith.kumar@mind-graph.com>
2023-09-28 12:38:01 +0530

    First Commit

--- src/main/java/com/example/demo/model/DemoEntity.java
+++ src/main/java/com/example/demo/model/DemoEntity.java
@@ -27,28 +27,4 @@ public class DemoEntity {
 	@Column(name = "Department")
 	private String department;
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
 }
